b0994f8f18e4f4fecca9b09fdbccf7476e516146
Aizachan <janlayaiza@gmail.com>
2025-11-18 08:09:27 +0800

    added new function for math.js arithmetics

---
+++ math.js
@@ -0,0 +1,44 @@
+// let n1 = 5;
+// let n2 = 10;
+
+// export function add() {
+//     return n1 + n2;
+// }
+// export function multiply() {
+//     return n1 * n2;
+// }
+// export function subtract() {
+//     return n2 - n1;
+// }
+// export function divide() {
+//     return n2 / n1;
+// }
+
+    
+// console.log("Addition:", add());
+// console.log("Multiplication:", multiply());
+// console.log("Subtraction:", subtract());
+// console.log("Division:", divide());
+num1=2;
+num2=3;
+
+function sum(num1,num2){
+    sum=num1+num2;
+    console.log("sum:",sum);
+}
+function multiply(num1,num2){
+    multiply=num1*num2;
+    console.log("multiply:",multiply);
+}
+function diff(num1,num2){
+    diff=num2-num1;
+    console.log("subtract:",diff);
+}
+function divide(num1,num2){
+    divide=num2/num1;
+    console.log("divide:",divide);
+}
+sum(num1,num2);
+multiply(num1,num2);
+diff(num1,num2);
+divide(num1,num2);

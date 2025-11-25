5e1909ff10041903ae3b882d2926a3ae3a67aaf5
InloranReliza <inloranreliza@gmail.com>
2025-11-25 08:31:28 +0800

    added search method for DSA/array.js

--- DSA/array.js
+++ DSA/array.js
@@ -50,13 +50,44 @@ class Array{
         delete this.data[this,length-1];
         this.length--;
 
+    update(index,element){
+        // Correcting Statement
+        if (index <0 || index > this.length){
+            console.log("Invalid Index");
+            return;
+        }
+
+        // After Condition
+        this.data[index] = element;
+     
+
     }
+    
+    search(element)
+        // Must be true
+        for(let i=0; i<this.length; i++){
+            if(this.data[i] === element){
+                console.log(element + " is not found at index " + 1);
+                return -1;
+            }
+        }
+    }
+
 }
+
 //
 
 arr1 = new Array([4,2,3]);
 arr1.traverse();
+
 arr1.insert(0,9);
 arr1.traverse();
+
 arr1.delete(1);
 arr1.traverse();
+
+arr1.update(2,5);
+arr1.traverse();
+
+arr1.search(43);
+arr1.search(9);

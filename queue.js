9e908e363c642bb4fe3786034d54aaa6434f6474
Aizachan <janlayaiza@gmail.com>
2025-12-02 08:03:46 +0800

    added utility methods for DSA/queue.js

---
+++ queue.js
@@ -0,0 +1,45 @@
+class Queue{
+    constructor(){
+        this.data = {}
+        this.frontIndex = 0;
+        this.rearIndex = -1;
+    }
+
+    enqueue(element){
+        this.rearIndex;
+        this.data[this.rearIndex] = element;
+    }
+
+    traverse(){
+        let output = "";
+        for (let i = this.frontIndex; i<= this.rearIndex; i++){
+            output += this.data[i] + "";
+        }
+        return output;
+    }
+
+    is_empty(){
+        return this.frontIndex > this.rearIndex;
+    }
+    front(){
+        //correction statement
+        if (this.is_empty()) return null;
+        return this.data[this.frontIndex];
+    }
+    size(){
+        return this.rearIndex - this.frontIndex +1;
+    }
+}
+//
+
+let queue1 = new Queue();
+console.log(queue1.is_empty());
+queue1.enqueue(4);
+console.log("The current queue size is:" + queue1.siza());
+queue1.enqueue(2);
+console.log("The current queue size is:" + queue1.siza());
+queue1.enqueue(3);
+console.log("The current queue size is:" + queue1.siza());
+console.log(queue1.is_empty());
+console.log(queue1.front());
+console.log("The current queue size is:" + queue1.siza());
